532a2a96922a1bdfe41f7a1db653675546b1ec8e
yamunanavada <yamunanavada@gmail.com>
2018-02-08 12:31:21 -0500

    added prelim code and folders

---
+++ src/containers/DishlisterContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class DishlisterContainer extends React.Component { 
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+
+    )
+  }
+ }
+export default DishlisterContainer
---
+++ src/containers/MyDishlisterContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyDishlisterContainer extends React.Component { 
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+
+    )
+  }
+ }
+export default MyDishlisterContainer
