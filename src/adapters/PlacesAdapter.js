0253f6028f8cebbf00e36f62188e2441cb9f0e4d
eal6gf <elum1234@gmail.com>
2018-02-08 17:04:55 -0500

    places api

---
+++ src/adapters/PlacesAdapter.js
@@ -0,0 +1,8 @@
+class PlacesAdapter {
+  //Adapter for Google Maps Places API for restaurants.
+  getPlaceInfo(latitude, longitude){
+    const apiKey = process.env.REACT_APP_PLACES_API_KEY
+    const URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},%20${longitude}&radius=500&type=restaurant&key=${apiKey}`
+    return fetch(URL).then(res => res.json())
+  }
+}
