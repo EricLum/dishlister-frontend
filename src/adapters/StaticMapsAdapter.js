f2cf73412961508a540929a53df17e698e0a50cc
yamunanavada <yamunanavada@gmail.com>
2018-02-09 11:21:58 -0500

    adding more stuff

--- src/adapters/StaticMapsAdapter.js
+++ src/adapters/StaticMapsAdapter.js
@@ -1,10 +1,10 @@
 class StaticMapsAdapter {
   //Google Geocoding API
   static getCoordinates(address) {
-  debugger
-  let cleanAddress = address.split(' ').join('%20')
-  const apiKey = process.env.REACT_APP_API_KEY
-  const URL = `https://maps.googleapis.com/maps/api/geocode/json?&address=${cleanAddress}&key=${apiKey}`
-  return fetch(URL).then(res => res.json())
+
+    let cleanAddress = address.split(' ').join('%20')
+    const apiKey = process.env.REACT_APP_API_KEY
+    const URL = `https://maps.googleapis.com/maps/api/geocode/json?&address=${cleanAddress}&key=${apiKey}`
+    return fetch(URL).then(res => res.json())
   }
 }
--- src/containers/DishlisterContainer.js
+++ src/containers/DishlisterContainer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import RestaurantList from './RestaurantList'
 import RestaurantSearch from '../components/RestaurantSearch'
+import PlacesAdapter from '../adapters/PlacesAdapter'
+import StaticMapsAdapter from '../adapters/StaticMapsAdapter'
 
 
 class DishlisterContainer extends React.Component {
@@ -8,10 +10,41 @@ class DishlisterContainer extends React.Component {
     super(props);
 
     this.state = {
-
+      startingAddress: {},
+      clickedRestaurants: []
     }
 
   }
+
+  fetchStaticGoogleMaps = (address) => {
+
+    let cleanAddress = address.split(' ').join('%20')
+    const apiKey = process.env.REACT_APP_API_KEY
+    const URL = `https://maps.googleapis.com/maps/api/geocode/json?&address=${cleanAddress}&key=${apiKey}`
+    return fetch(URL).then(res => res.json())
+  }
+
+
+
+  fetchPlaces = () => {
+    const apiKey = process.env.REACT_APP_PLACES_API_KEY
+    const URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${40.7733832},${-73.94863389999999}&radius=500&type=restaurant&key=${apiKey}`
+    return fetch(URL).then(res => res.json()).then(console.log)
+
+  }
+
+
+  componentDidMount = () => {
+    this.fetchStaticGoogleMaps("1556 York Avenue, New York, NY 10028").then(res => {
+      console.log(res)
+      this.setState({
+        startingAddress: res.results[0].geometry.location
+      }, () => {console.log(this.state)})
+    }).then(this.fetchPlaces())
+  }
+
+
+
 
 
 
